392aa3789ca4b7f1cf6c43c5dfbe7eb41af36610
jara505 <juanignaciojara505@gmail.com>
2026-02-19 23:09:45 -0600

    feat: add custom JWT claims with username and email

--- roadmaps/DRF/django/src/core/settings.py
+++ roadmaps/DRF/django/src/core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework",
     "rest_framework_simplejwt",
-    "rest_framework_simplejwt.token_backlist",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
